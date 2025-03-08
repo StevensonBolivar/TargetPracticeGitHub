aaa62d8274f3a4d3c5b911d2fb14a17c1111b6ab
Stevenson Bolivar <stevensonbolivar@gmail.com>
2025-03-08 07:13:50 -0500

    Upload Initial Version

---
+++ Main.R
@@ -0,0 +1,34 @@
+#Txt_File |> filter(str_detect(ID,"[a-zA-Z]+[Cc]hen")) |> View()
+library(tidyverse)
+
+source(str_c("RCode\\",dir("RCode"),sep=""))
+
+FileName <- "Data\\citations.txt"
+Citations <- LoadCitations(FileName) 
+Citations2 <- Citations |> 
+  CleanStep1Df() 
+CitationsW <- Citations2 |> 
+  WiderCitations() 
+CitationsList <-  CitationsW |> 
+  GenID_Df() 
+
+ResFinal <- PrintRefDf(CitationsList)
+
+library(targets) 
+library(tarchetypes)
+use_targets()
+
+tar_manifest()
+tar_visnetwork()
+tar_make()
+tar_meta(fields = "error") |> drop_na() |> pull()
+
+#tar_destroy()
+library(tidyverse)
+tar_source("RCode")
+CitationsList <- tar_read(CitationsList)
+
+PrintRef(CitationsList[17,])
+cat(PrintRef(CitationsList[17,]))
+
+ResFinal <- tar_read(ResFinal)
---
+++ RCode/Functions.R
@@ -0,0 +1,72 @@
+LoadCitations <- function(TxtFile){
+  Df <- tibble(read_delim(TxtFile,"\t",col_names = FALSE))
+}
+
+CleanStep1Df <- function(Df){
+  Df |> filter(!str_detect(X1,"^%"))
+}
+
+WiderCitations <- function(Df){
+  Df |>
+    mutate(
+      X1 = str_replace(X1, "@([a-zA-Z0-9]+)\\{(.+),", "Type=\\1@\\2") # Replace @x2Name with Name
+    ) |> 
+    separate(X1, into = c("X1", "ID"), sep = "@") |>
+    separate(X1, into = c("VarName", "Values"), sep = "=") |> 
+    mutate(
+      VarName=str_trim(VarName) |> 
+        str_replace_all("[\\}\\{],?",""),
+      Values=str_trim(Values) |> 
+        str_replace_all("[\\}\\{],?","")
+    ) |> 
+    filter(!is.na(Values)) |> 
+    fill(ID, .direction = "down") |> 
+    distinct() |> 
+    pivot_wider(id_cols=ID , names_from = VarName,values_from = Values,values_fn = list(value = first)) |> 
+    select(ID, Type, title,author,journal,pages,year)
+}
+
+GenID <- function(Names,Year){
+  if(str_detect(Names,",")){
+    ID <- Names |> 
+      str_split("and") |> 
+      unlist() |> 
+      str_trim() |> 
+      map(~ .x |> str_split(",") |> unlist() |> head(-1) |> str_c(collapse = "")) |> 
+      str_c(collapse = "") |> 
+      str_c(Year, collapse = "")
+  }else{
+    ID <- Names |> 
+      str_split("and") |> 
+      unlist() |> 
+      str_trim() |> 
+      map(~ .x |> str_split(" ") |> unlist() |> tail(-1) |> str_c(collapse = "")) |> 
+      str_c(collapse = "") |> 
+      str_c(Year, collapse = "")
+  }
+  ID |> str_remove_all("[^a-zA-Z0-9]")
+}
+
+GenID_Df <- function(Df) {
+  Df |> mutate(ID2=map2(author,year,~GenID(.x,.y)))
+} 
+
+PrintRef <- function(Df){
+  TMP=str_glue("@{Df$Type}{{{Df$ID2}}},
+    title={{{Df$title}}},
+    author={{{Df$author}}},
+    journal={{{Df$journal}}},
+    pages={{{Df$pages}}},
+    year={{{Df$year}}}
+  }")
+  TMP |> str_remove_all("NULL")
+}
+
+PrintRefDf <- function(Df,FileName="NewCitations"){
+  TMP= map(1:nrow(Df),~PrintRef(Df[.x,])) 
+  dir.create("Results", showWarnings = FALSE)  # Ensure the folder exists
+  full_path <- file.path("Results", FileName) 
+  write_lines(TMP,full_path)
+  return(full_path)
+}
+
